b585daae45c9e52f19924aba97a233d1d38dc0dd
Julian Hille <julian@hille.family>
2022-06-24 11:04:33 +0200

    Add some tests for appending pages

--- tests/AppendPagesTest.js
+++ tests/AppendPagesTest.js
@@ -2,7 +2,7 @@ const muhammara = require('../muhammara')
 const fs = require("fs");
 
 describe('AppendPagesTest', function() {
-	it('should complete without error', function() {
+	it('with files should complete without error', function() {
 		var pdfWriter = muhammara.createWriter(__dirname + '/output/AppendPagesTest.pdf');
 		pdfWriter.appendPDFPagesFromPDF(__dirname + '/TestMaterials/Original.pdf');
 		pdfWriter.appendPDFPagesFromPDF(__dirname + '/TestMaterials/XObjectContent.PDF');
@@ -10,10 +10,8 @@ describe('AppendPagesTest', function() {
 
 		pdfWriter.end();
 	});
-});
 
-describe('AppendPagesTest with stream', function() {
-	it('should complete without error', function() {
+	it('with a read stream and write to a new file should complete without error', function() {
 		var pdfWriter = muhammara.createWriter(__dirname + '/output/AppendPagesTest2.pdf');
 		const mainStream = new muhammara.PDFRStreamForFile(__dirname + '/TestMaterials/Original.pdf');
 		pdfWriter.appendPDFPagesFromPDF(mainStream);
@@ -22,12 +20,8 @@ describe('AppendPagesTest with stream', function() {
 
 		pdfWriter.end();
 	});
-});
 
-
-describe('AppendPagesTest with stream', function() {
-	it('should complete without error', function() {
-		var fs = require('fs')
+	it('with a read stream and a file and modification in place should complete without error', function() {
 		fs.copyFileSync(__dirname + '/TestMaterials/Original.pdf', __dirname + '/output/AppendPagesTest3Original.pdf')
 		var pdfWriter = muhammara.createWriterToModify(
 			__dirname + '/output/AppendPagesTest3Original.pdf'
@@ -40,11 +34,8 @@ describe('AppendPagesTest with stream', function() {
 
 		pdfWriter.end();
 	});
-});
 
-
-describe('AppendPagesTest with streams on both ends', function() {
-	it('should complete without error', function() {
+	it('with a read stream and an output stream and modification in place should complete without error', function() {
 		var fs = require('fs')
 		fs.copyFileSync(__dirname + '/TestMaterials/Original.pdf', __dirname + '/output/AppendPagesTest4Original.pdf')
 		var pdfWriter = muhammara.createWriterToModify(
@@ -60,4 +51,3 @@ describe('AppendPagesTest with streams on both ends', function() {
 		pdfWriter.end();
 	});
 });
-
