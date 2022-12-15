94991118ff1b888ab9c0eff144bf474555773a7a
Julian Hille <julian@hille.family>
2022-12-16 00:37:09 +0100

    First draft injecting muhammara-recipe

---
+++ tests/recipe/images.js
@@ -0,0 +1,91 @@
+const path = require('path');
+const HummusRecipe = require('../../lib').Recipe;
+
+describe('Modify', () => {
+    const taskAI = 'Add images';
+    it(taskAI, (done) => {
+        const src = path.join(__dirname, 'materials/test.pdf');
+        const output = path.join(__dirname, `output/${taskAI}.pdf`);
+        const myCats = path.join(__dirname, 'materials/myCats.jpg');
+        const wiki = path.join(__dirname, 'materials/wiki.png');
+
+        const recipe = new HummusRecipe(src, output);
+        recipe
+            .editPage(1)
+            .image(myCats, 'center', 'center', {
+                width: 300,
+                height: 300,
+                keepAspectRatio: false,
+                opacity: 1,
+                align: 'center center'
+            })
+            .image(myCats, 500, 210, {
+                scale: 0.1,
+                rotation: -90
+            })
+            .rectangle(500, 210, 73, 97, {
+                rotation: -90,
+                color: '#ff0000'
+            })
+            .image(myCats, 'center', 600, {
+                scale: 0.1,
+                align: 'center center',
+                rotation: 45,
+                rotationOrigin: [270, 550],
+                skewY: 10
+            })
+            .rectangle(270, 550, 73, 97, {
+                rotation: 45,
+                rotationOrigin: [270, 550],
+                skewY: 10
+            })
+            .image(wiki, 0, 0, {
+                width: 300,
+                height: 300,
+                opacity: 0.7
+            })
+            .endPage()
+            .editPage(2)
+            .rectangle(0, 0, 800, 800, {
+                color: [22, 48, 9]
+            })
+            .image(wiki, 'center', 'center', {
+                width: 300,
+                height: 300,
+                align: 'center center'
+            })
+            .endPage()
+            .endPDF(done);
+    });
+
+    const taskATP = 'Add transparent png';
+    it(taskATP, (done) => {
+        const src = path.join(__dirname, 'materials/test.pdf');
+        const output = path.join(__dirname, `output/${taskATP}.pdf`);
+        const wikiPng = path.join(__dirname, 'materials/wiki.png');
+
+        const recipe = new HummusRecipe(src, output);
+        recipe
+            .editPage(1)
+            .image(wikiPng, 'center', 50, {
+                width: 300,
+                height: 300,
+                align: 'center center',
+                opacity: 1,
+            })
+            .image(wikiPng, 'center', 250, {
+                width: 300,
+                height: 300,
+                align: 'center center',
+                opacity: 0.6,
+            })
+            .image(wikiPng, 'center', 450, {
+                width: 300,
+                height: 300,
+                align: 'center center',
+                opacity: 0.3,
+            })
+            .endPage()
+            .endPDF(done);
+    });
+});
---
+++ tests/recipe/text-rotation.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HummusRecipe = require('../../lib').Recipe;
+
+describe('Text Rotation', () => {
+    it('Add text with rotation', (done) => {
+        const src = path.join(__dirname, 'materials/test.pdf');
+        const output = path.join(__dirname, 'output/Add text - rotation1.pdf');
+        const recipe = new HummusRecipe(src, output);
+
+        const pages = recipe.metadata.pages;
+        const angles = [0, 0, 1, 45, -45, 90, 135, -135, 180, 270, 360, -90, -180, -270, -360, 450];
+        for (let i = 1; i <= pages; i++) {
+            const angle = angles[i] || 0;
+            recipe
+                .editPage(i)
+                .circle('center', 300, 10, {
+                    stroke: '#0032FF'
+                })
+                .text(`${angle} ROTATION pjqy`, 'center', 300, {
+                    bold: true,
+                    size: 80,
+                    color: '#0000FF',
+                    align: 'center center',
+                    rotation: angle,
+                    opacity: 0.25
+                })
+                .circle('center', 'center', 10, {
+                    stroke: '#0032FF'
+                })
+                .text(`${angle} ROTATION pjqy`, 'center', 'center', {
+                    bold: true,
+                    size: 80,
+                    color: '#0000FF',
+                    align: 'center center',
+                    rotation: angle,
+                    opacity: 0.5
+                })
+                .circle('center', 500, 10, {
+                    stroke: '#0032FF'
+                })
+                .text(`${angle} ROTATION pjqy`, 'center', 500, {
+                    bold: true,
+                    size: 80,
+                    color: '#0000FF',
+                    align: 'center center',
+                    rotation: angle
+                })
+                .endPage();
+        }
+        recipe.endPDF(done);
+    });
+});
