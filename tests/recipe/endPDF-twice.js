06c7fa98c9821469632bf6158d7ed875a6673d22
Sergey Stepanov <sergey.stepanov@honeycombinsurance.com>
2025-12-18 13:11:31 +0200

    fix: prevent crash when endPDF is called twice
    Add guard to throw a descriptive error instead of causing a
    segmentation fault when endPDF is called multiple times.

---
+++ tests/recipe/endPDF-twice.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const { expect } = require("chai");
+const HummusRecipe = require("../../lib").Recipe;
+
+describe("endPDF called twice", () => {
+  it("should fail when endPDF is called twice", (done) => {
+    const output = path.join(__dirname, "../output/endPDF-twice.pdf");
+    const recipe = new HummusRecipe("new", output);
+
+    recipe.createPage("letter").endPage().endPDF();
+
+    expect(() => {
+      recipe.endPDF();
+    }).to.throw();
+
+    done();
+  });
+});
