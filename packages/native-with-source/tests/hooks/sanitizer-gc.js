445394e29b74f3b8780367e7a06b0ae62204cf02
Julianhille <julian@hille.family>
2026-09-03 23:58:07 +0200

    Enable garbage collection in Electron tests

--- packages/native-with-source/tests/hooks/sanitizer-gc.js
+++ packages/native-with-source/tests/hooks/sanitizer-gc.js
@@ -2,6 +2,9 @@
 
 exports.mochaHooks = {
   afterAll: function () {
+    if (typeof global.gc !== "function") {
+      throw new Error("GC must be exposed for this test run");
+    }
     global.gc();
     global.gc();
   },
