a4cb4a00445545ff9947150fb63d8382dee6a616
Julianhille <julian@hille.family>
2026-09-14 00:58:47 +0200

    Fix writer cleanup after end failure

--- packages/native-with-source/src/PDFWriterDriver.cpp
+++ packages/native-with-source/src/PDFWriterDriver.cpp
@@ -149,12 +149,6 @@ METHOD_RETURN_TYPE PDFWriterDriver::End(const ARGS_TYPE& args)
     pdfWriter->mPDFWriter.GetDocumentContext().RemoveDocumentContextExtender(pdfWriter);
 
 
-    if(status != PDFHummus::eSuccess)
-    {
-		THROW_EXCEPTION("Unable to end PDF");
-		SET_FUNCTION_RETURN_VALUE(UNDEFINED)
-    }
-
     if(pdfWriter->mWriteStreamProxy)
     {
         delete pdfWriter->mWriteStreamProxy;
@@ -168,6 +162,12 @@ METHOD_RETURN_TYPE PDFWriterDriver::End(const ARGS_TYPE& args)
     }
 
     pdfWriter->mIsStarted = false;
+
+    if(status != PDFHummus::eSuccess)
+    {
+		THROW_EXCEPTION("Unable to end PDF");
+		SET_FUNCTION_RETURN_VALUE(UNDEFINED)
+    }
 
     SET_FUNCTION_RETURN_VALUE(args.This())
 }
