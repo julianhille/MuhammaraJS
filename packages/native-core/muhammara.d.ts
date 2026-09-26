ecdb61ebcdc1a8abf697738c92541d8fc60d4d03
Julianhille <julian@hille.family>
2026-09-26 14:07:39 +0200

    Document AbstractContentContext K

--- packages/native-core/muhammara.d.ts
+++ packages/native-core/muhammara.d.ts
@@ -615,6 +615,16 @@ declare namespace muhammara {
      * @throws {TypeError} If there is no content context.
      */
     rg(r: number, g: number, b: number): this;
+    /**
+     * Sets a CMYK stroke color; operator K.
+     * @param c - Cyan, 0 to 1.
+     * @param m - Magenta, 0 to 1.
+     * @param y - Yellow, 0 to 1.
+     * @param k - Black, 0 to 1.
+     * @returns This context.
+     * @throws {TypeError} If the arguments are not 4 numbers.
+     * @throws {TypeError} If there is no content context.
+     */
     K(c: number, m: number, y: number, k: number): this;
     k(c: number, m: number, y: number, k: number): this;
     W(): this;
