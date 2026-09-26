68b5f6f95a4150dd77be54bee8669c1f4f67ece4
Julianhille <julian@hille.family>
2026-09-26 14:05:11 +0200

    Document Wasm DocumentCopyingContext.createFormXObjectFromPDFPage()

--- packages/wasm/lib/writer.js
+++ packages/wasm/lib/writer.js
@@ -4365,6 +4365,16 @@ export function createWriterFactory({
             }
             return this;
           },
+          /**
+           * Creates a form XObject from a source page.
+           * @param {number} index - Zero-based source page index.
+           * @param {PDFPageBoxType|PDFRectangle} [pageBox=ePDFPageBoxMediaBox] - Box used as the form bounds, or a rectangle.
+           * @param {PDFMatrix} [transformation] - Form matrix.
+           * @returns {number} Object ID of the form.
+           * @throws {TypeError} If a rectangle or matrix is not finite.
+           * @throws {RangeError} If `index` or `pageBox` is invalid or the form cannot be created.
+           * @throws {Error} If the writer or the copying context has ended.
+           */
           createFormXObjectFromPDFPage: function (
             index,
             pageBox = constants.ePDFPageBoxMediaBox,
